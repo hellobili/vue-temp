fb3a7017ae75dec4ca768db365f995b2b1476d92
jiya <742393511@qq.com>
2020-11-04 11:09:42 +0800

    add commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './components'
+import './components' // 全局组件组册
 
 Vue.config.productionTip = false
 
