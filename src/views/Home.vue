4b63c2a81a50b8e43921301108930426ec333466
jiya <742393511@qq.com>
2020-11-04 11:07:27 +0800

    feat: add vuex modules

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <div> store in app 'appName' : {{appName}}</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
@@ -13,6 +15,9 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  computed: {
+    ...mapState('app', ['appName'])
   }
 }
 </script>
