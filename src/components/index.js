037666a03a76b111a2e61b998d33630beb278b87
大宝 <742393511@qq.com>
2020-11-25 11:50:44 +0800

    rf: update components common name

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
  * 注册全局组件
  * 自动引入 modules 下各组件内的 js文件
  */
-const componentsContext = require.context('./modules', true, /\.js$/)
+const componentsContext = require.context('./common', true, /\.js$/)
 
 componentsContext.keys().forEach(component => {
   const componentConfig = componentsContext(component)
